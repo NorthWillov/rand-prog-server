58df5e76d77fa57c780e9b929f1ac1c111b7a99d
NorthWillov <northwillov@gmail.com>
2023-03-03 17:36:02 +0100

    Fix mongoose var

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require('cors');
